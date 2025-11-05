97f8f4122dfe58cc6de2a09e9e081a467d90eb8f
Ruthie Willow <rwillowb@gmail.com>
2025-11-05 11:41:43 -0800

    Testing running w/o Docker

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -17,6 +17,36 @@ class PasswordTest : public ::testing::Test
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+}
+
+TEST(PasswordTest, two_leding_letters)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("aab"));
+}
+TEST(PasswordTest, no_repeats)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("acab"));
+}
+TEST(PasswordTest, leading_number)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("11234"));
+}
+TEST(PasswordTest, case_sensitive)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("AaAaAa"));
+}
+TEST(PasswordTest, empty)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters(""));
+}
+TEST(PasswordTest, symbols)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("$$$#"));
 }
